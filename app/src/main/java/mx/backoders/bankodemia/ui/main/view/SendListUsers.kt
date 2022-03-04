e357447df71e680d3c84307c150f1a56dfdee623
Diego2108 <72366044+Diego2108@users.noreply.github.com>
2022-03-04 08:53:17 -0600

    pantallas sendEdit y SendLisUsers

---
+++ app/src/main/java/mx/backoders/bankodemia/ui/main/view/SendListUsers.kt
@@ -0,0 +1,11 @@
+package mx.backoders.bankodemia.ui.main.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SendListUsers : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_send_list_users)
+    }
+}
