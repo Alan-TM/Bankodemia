e4bdcf334db069e14e83589bc4d0fc0fa08b9ecd
Diego2108 <72366044+Diego2108@users.noreply.github.com>
2022-03-07 21:54:12 -0600

    pantallas finalizado

---
+++ app/src/main/java/mx/backoders/bankodemia/Processing_Transaction.kt
@@ -0,0 +1,11 @@
+package mx.backoders.bankodemia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Processing_Transaction : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_processing_transaction)
+    }
+}
