3813b9639d966fea07263ccd4b48b9dbe543528c
Alan <alantorresm86@gmail.com>
2022-03-11 14:54:31 -0700

    cleaning code

--- app/src/main/java/mx/backoders/bankodemia/common/api/RetrofitInstance.kt
+++ app/src/main/java/mx/backoders/bankodemia/common/api/RetrofitInstance.kt
@@ -5,6 +5,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
+private const val API_URL = "https://bankodemia.kodemia.mx/"
+
 object RetrofitInstance {
     fun getRetrofit(): Retrofit {
         val okHttpClient = OkHttpClient.Builder()
@@ -12,7 +14,7 @@ object RetrofitInstance {
             .connectTimeout(60, TimeUnit.SECONDS)
             .build()
         return Retrofit.Builder()
-            .baseUrl("https://bankodemia.kodemia.mx/")
+            .baseUrl(API_URL)
             .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
