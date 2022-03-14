ff2eb18cc5ef10b1bc0a70554d1c26f0ef7ea5a8
car10saletsis <car10s@live.com.mx>
2022-03-13 20:54:59 -0600

    fragment login

---
+++ app/src/main/java/mx/backoders/bankodemia/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package mx.backoders.bankodemia.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
