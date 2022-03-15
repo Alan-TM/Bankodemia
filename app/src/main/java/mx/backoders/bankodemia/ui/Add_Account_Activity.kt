514bbb4d43fda39ea874db43b22a33071ddd1361
Roberto Herrera <robeherrera561.2@gmail.com>
2022-03-14 22:54:58 -0600

    Refactor project

--- app/src/main/java/mx/backoders/bankodemia/ui/Add_Account_Activity.kt
+++ app/src/main/java/mx/backoders/bankodemia/ui/Add_Account_Activity.kt
@@ -3,7 +3,6 @@ package mx.backoders.bankodemia.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import mx.backoders.bankodemia.R
-import mx.backoders.bankodemia.databinding.ActivityActivityLoginBinding
 
 class Add_Account_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
