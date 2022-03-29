2c766a2f430343dd4fcff27f9945aa15c075c633
Roberto Herrera <robeherrera561.2@gmail.com>
2022-03-28 23:03:28 -0600

    Correct Login and verification of fields

--- app/src/main/java/mx/backoders/bankodemia/ui/login/view/LoginFragment.kt
+++ app/src/main/java/mx/backoders/bankodemia/ui/login/view/LoginFragment.kt
@@ -26,8 +26,13 @@ class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+    val loginViewModel: LoginViewModel by viewModels()
+    lateinit var shared: SharedPreferencesInstance
 
-
+    private lateinit var tietEmail: TextInputEditText
+    private lateinit var tilEmail: TextInputLayout
+    private lateinit var tietPassword: TextInputEditText
+    private lateinit var tilPassword: TextInputLayout
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -41,15 +46,72 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initializeUI()
+        loginObservers()
     }
 
     private fun initializeUI() {
+        shared = SharedPreferencesInstance.getInstance(requireActivity().applicationContext)
+
+        //Adding Listeners
+        tietEmail = binding.loginEdittextEmailTiet
+        tilEmail = binding.loginEdittextEmailTil
+        addIsEmailCorrectListener(requireActivity().getApplicationContext(), tietEmail, tilEmail)
+
+        tietPassword = binding.loginEdittextPasswordTiet
+        tilPassword = binding.loginEdittextPasswordTil
+        addIsEmptyChecker(requireActivity().getApplicationContext(), tietPassword, tilPassword)
+
         binding.returnLogin.setOnClickListener {
             findNavController().navigateUp()
         }
         binding.loginLogin.setOnClickListener {
-            openHomeActivity()
+            startLogIn()
         }
+    }
+
+    private fun startLogIn() {
+        isEmailCorrect(requireActivity().getApplicationContext(), tietEmail, tilEmail)
+        isEmpty(requireActivity().getApplicationContext(), tietPassword, tilPassword)
+
+        if (!tilEmail.isErrorEnabled && !tilPassword.isErrorEnabled) {
+            val email = tietEmail.text.toString()
+            val pass = tietPassword.text.toString()
+            loginViewModel.login(LoginDto(email, pass))
+        } else {
+            when {
+                tietEmail.text!!.isEmpty() -> tilEmail.requestFocus()
+                tietPassword.text!!.isEmpty() -> tilPassword.requestFocus()
+            }
+        }
+    }
+
+    private fun loginObservers() {
+        loginViewModel.login.observe(viewLifecycleOwner) { login ->
+            shared.saveSession(login)
+        }
+        loginViewModel.tokenExpired.observe(viewLifecycleOwner) { tokenExpirado ->
+            if (tokenExpirado) {
+                // regresarlo al login
+            }
+        }
+        loginViewModel.success.observe(viewLifecycleOwner) { success ->
+            if(success){
+                logi("Robe send Actividiti:" )
+                openHomeActivity()
+            }else{
+                logi("Robe dont sent activitu ERROR" )
+                showErrorMessage()
+            }
+        }
+    }
+
+    private fun showErrorMessage() {
+        Toast.makeText(context, "ERROR CREDENTIALS", Toast.LENGTH_LONG).show()
+    }
+
+    fun openHomeActivity() {
+        val intent = Intent(activity, HomeActivity::class.java)
+        startActivity(intent)
     }
 //    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 //        super.onViewCreated(view, savedInstanceState)
@@ -116,10 +178,5 @@ class LoginFragment : Fragment() {
 //        Toast.makeText(appContext, "ERROR CREDENTIALS", Toast.LENGTH_LONG).show()
 //    }
 //
-    fun openHomeActivity() {
-        val intent = Intent(activity, HomeActivity::class.java)
-        startActivity(intent)
-    }
-
 }
 
