23307d004cec3155e661a3d8229b9ecd9fc4001a
car10saletsis <car10s@live.com.mx>
2022-03-29 18:02:16 -0600

    ÂNav 60%

--- app/src/main/java/mx/backoders/bankodemia/ui/main/WelcomeActivity.kt
+++ app/src/main/java/mx/backoders/bankodemia/ui/main/WelcomeActivity.kt
@@ -3,10 +3,17 @@ package mx.backoders.bankodemia.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import mx.backoders.bankodemia.R
+import mx.backoders.bankodemia.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWelcomeBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setContentView(R.layout.activity_welcome)
     }
 }
