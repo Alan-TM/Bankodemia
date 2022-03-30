5fc8db57529a72bde13e170cfe724ef4723943f2
Roberto Herrera <robeherrera561.2@gmail.com>
2022-03-29 23:43:04 -0600

    Merger Working App Checkpoint 6

--- app/src/main/java/mx/backoders/bankodemia/ui/main/HomeActivity.kt
+++ app/src/main/java/mx/backoders/bankodemia/ui/main/HomeActivity.kt
@@ -3,7 +3,6 @@ package mx.backoders.bankodemia.ui.main
 import android.os.Build
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
@@ -19,51 +18,50 @@ import mx.backoders.bankodemia.databinding.ActivityHomeBinding
 import mx.backoders.bankodemia.ui.home.viewmodel.HomeViewModel
 import mx.backoders.bankodemia.ui.transactions.viewmodel.TransactionsViewModel
 
-//@RequiresApi(Build.VERSION_CODES.O)
+@RequiresApi(Build.VERSION_CODES.O)
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
-//    private val viewModel: HomeViewModel by viewModels()
-//    private val makeTransactionViewModel: TransactionsViewModel by viewModels()
-//
+    private val viewModel: HomeViewModel by viewModels()
+    private val makeTransactionViewModel: TransactionsViewModel by viewModels()
+
 //    private lateinit var sharedPreferences: SharedPreferencesInstance
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        Toast.makeText(applicationContext,"",Toast.LENGTH_SHORT).show()
-//
-//
-//        //this should be added in the login activity
+
+
+        //this should be added in the login activity
 //        sharedPreferences = SharedPreferencesInstance.getInstance(this)
 //        sharedPreferences.saveSession(UserLoginResponse("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MjFmYTNmYjhjZTZjNDc4ZDBlMWI5OTEiLCJpYXQiOjE2NDg0ODgxMTUsImV4cCI6MTY0ODU3NDUxNX0.69ZgcnR781dtTPEFl-9yXn5go5bAaRz9oV8ff7j3t-I", "24h"))
-//        //sharedPreferences.getPreference("token")?.let{ Log.e("SharedPreferences", it) }
-//        //sharedPreferences.getPreference("expiresIn")?.let{ Log.e("SharedPreferences", it) }
-//
-//        navigationSetup()
-//        initializeObservers()
+        //sharedPreferences.getPreference("token")?.let{ Log.e("SharedPreferences", it) }
+        //sharedPreferences.getPreference("expiresIn")?.let{ Log.e("SharedPreferences", it) }
+
+        navigationSetup()
+        initializeObservers()
     }
 
-//    private fun initializeObservers(){
-//        with(viewModel) {
-//            bottomNavIsVisible.observe(this@HomeActivity) { binding.navView.isVisible = it }
-//            topToolbarIsVisible.observe(this@HomeActivity) { binding.actionBar.isVisible = it }
-//            androidNavigationBarIsVisible.observe(this@HomeActivity) { setHideAndroidNavigationBar(it) }
-//        }
-//    }
-//
-//    private fun navigationSetup(){
-//        val navView: BottomNavigationView = binding.navView
-//        val navController = findNavController(R.id.nav_host_fragment_activity_home)
-//        navView.setupWithNavController(navController)
-//    }
-//
-//    private fun setHideAndroidNavigationBar(hide: Boolean){
-//        if(hide){
-//            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-//        } else{
-//            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
-//        }
-//    }
+    private fun initializeObservers(){
+        with(viewModel) {
+            bottomNavIsVisible.observe(this@HomeActivity) { binding.navView.isVisible = it }
+            topToolbarIsVisible.observe(this@HomeActivity) { binding.actionBar.isVisible = it }
+            androidNavigationBarIsVisible.observe(this@HomeActivity) { setHideAndroidNavigationBar(it) }
+        }
+    }
+
+    private fun navigationSetup(){
+        val navView: BottomNavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_activity_home)
+        navView.setupWithNavController(navController)
+    }
+
+    private fun setHideAndroidNavigationBar(hide: Boolean){
+        if(hide){
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        } else{
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+        }
+    }
 }
--- app/src/main/java/mx/backoders/bankodemia/ui/main/WelcomeActivity.kt
+++ app/src/main/java/mx/backoders/bankodemia/ui/main/WelcomeActivity.kt
@@ -16,60 +16,17 @@ import mx.backoders.bankodemia.ui.login.viewmodel.LoginViewModel
 class WelcomeActivity : AppCompatActivity() {
 
     private lateinit var bindingWelcomeActivity: ActivityWelcomeBinding
-    lateinit var shared: SharedPreferencesInstance
     private val loginViewModel: LoginViewModel by viewModels()
+//    private lateinit var sharedPreferences: SharedPreferencesInstance
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindingWelcomeActivity = ActivityWelcomeBinding.inflate(layoutInflater)
         setContentView(bindingWelcomeActivity.root)
-//        initComponents()
+//        initUI()
     }
 
-    private fun initComponents() {
-
-//        loginObservers()
-//        bindingWelcomeActivity.btnActLoginLogin.setOnClickListener {
-//            loginViewModel.welcomeContainerIsVisible(true)
-//
-//        }
-//        bindingWelcomeActivity.btnActLoginBack.setOnClickListener {
-//            loginViewModel.welcomeContainerIsVisible(false)
-//        }
-//
-//        bindingWelcomeActivity.btnActLoginSignup.setOnClickListener {
-//            val intent = Intent(this, SignUpActivity::class.java)
-//            startActivity(intent)
-//        }
-
-//        supportFragmentManager.beginTransaction().apply {
-//            replace(R.id.welcomeContainer, LoginFragment())
-//            commit()
-//        }
+    private fun initUI() {
+//        sharedPreferences = SharedPreferencesInstance.getInstance(applicationContext)
     }
-
-
-//    private fun loginObservers() {
-//        loginViewModel.liveDataWelcomeContainer.observe(this) { wecomeContainerIsVisible ->
-//            if (wecomeContainerIsVisible) {
-//                showLoginFragment()
-//            } else {
-//                hideLoginFragment()
-//            }
-//        }
-//    }
-//
-//    private fun showLoginFragment() {
-//        bindingWelcomeActivity.btnActLoginBack.visibility = View.VISIBLE
-//        bindingWelcomeActivity.btnActLoginLogin.visibility = View.GONE
-//        bindingWelcomeActivity.btnActLoginSignup.visibility = View.GONE
-//        bindingWelcomeActivity.welcomeContainer.visibility = View.VISIBLE
-//    }
-//
-//    private fun hideLoginFragment() {
-//        bindingWelcomeActivity.btnActLoginBack.visibility = View.GONE
-//        bindingWelcomeActivity.btnActLoginLogin.visibility = View.VISIBLE
-//        bindingWelcomeActivity.btnActLoginSignup.visibility = View.VISIBLE
-//        bindingWelcomeActivity.welcomeContainer.visibility = View.GONE
-//    }
 }
