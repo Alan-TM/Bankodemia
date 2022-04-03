47a7960261e4010acddac3d8be8ae534b630d923
Alan Torres <alantorresm86@gmail.com>
2022-04-03 13:17:28 -0600

    before changes to proper functionality

--- app/src/main/java/mx/backoders/bankodemia/ui/transactions/view/MakeTransactionFragment.kt
+++ app/src/main/java/mx/backoders/bankodemia/ui/transactions/view/MakeTransactionFragment.kt
@@ -5,18 +5,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
-import com.google.android.material.snackbar.Snackbar
 import mx.backoders.bankodemia.R
-import mx.backoders.bankodemia.common.utils.checkForInternet
-import mx.backoders.bankodemia.common.utils.errorMessageSelectorByCode
-import mx.backoders.bankodemia.common.utils.showSnack
 import mx.backoders.bankodemia.common.utils.PaymentType
 import mx.backoders.bankodemia.databinding.FragmentMakeTransactionBinding
+import mx.backoders.bankodemia.ui.home.viewmodel.HomeViewModel
 import mx.backoders.bankodemia.ui.transactions.viewmodel.TransactionsViewModel
 
 class MakeTransactionFragment : Fragment() {
@@ -24,12 +20,18 @@ class MakeTransactionFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val makeTransactionViewModel: TransactionsViewModel by activityViewModels()
+    private val homeViewModel: HomeViewModel by activityViewModels()
+
+    private lateinit var payment: PaymentType
 
     override fun onCreate(savedInstanceState: Bundle?) {
         arguments?.let {
+            payment = it.getSerializable("paymentType") as PaymentType
+
             makeTransactionViewModel.setContactInformation(
                 it.getString("contactID").toString(),
-                it.getString("contactFullName").toString()
+                it.getString("contactFullName").toString(),
+                payment
             )
         }
         super.onCreate(savedInstanceState)
@@ -53,8 +55,19 @@ class MakeTransactionFragment : Fragment() {
 
     private fun initializeObservers() {
         with(makeTransactionViewModel) {
-            contactFullName.observe(viewLifecycleOwner){ name ->
+            contactFullName.observe(viewLifecycleOwner) { name ->
                 binding.makeTransactionFullNameTextView.text = name
+            }
+
+            contactID.observe(viewLifecycleOwner) { id ->
+                binding.tvInterbankCodeFragmentMakeTransaction.text = id.substring(0..15)
+            }
+
+            transactionBody.observe(viewLifecycleOwner) { transactionBody ->
+                if (transactionBody.amount > 0) {
+                    binding.textInputLayoutQunatitySend.setText(transactionBody.amount.toString())
+                    binding.textInputConceptSend.setText(transactionBody.concept)
+                }
             }
         }
     }
@@ -62,34 +75,28 @@ class MakeTransactionFragment : Fragment() {
     private fun initializeUI() {
         with(binding) {
             makeTransactionBackButton.setOnClickListener {
+                makeTransactionViewModel.clearTransactionBodyStateHandle()
                 findNavController().navigateUp()
             }
 
             buttonMakeTransfer.setOnClickListener {
-                if (!checkForInternet(requireActivity().getApplicationContext())) {
-                    showSnack(
-                        binding.root,
-                        getString(R.string.error_no_internet),
-                        Snackbar.LENGTH_INDEFINITE
-                    )
-                } else {
-                    val amount = textInputLayoutQunatitySend.text.toString()
-                    val concept = textInputConceptSend.text.toString()
-                    if (makeTransactionViewModel.validateTextField(amount)) {
-                        textInputLayoutQunatitySend.error = null
-                        if (makeTransactionViewModel.validateTextField(concept)) {
-                            textInputConceptSend.error = null
-                            sendDataToMakeTransaction(amount, concept)
-                        } else {
-                            textInputConceptSend.error = getString(R.string.error_empty)
-                        }
-                    } else {
-                        textInputLayoutQunatitySend.error = getString(R.string.error_empty)
-                    }
-                }
+                val amount = textInputLayoutQunatitySend.text.toString()
+                val concept = textInputConceptSend.text.toString()
 
-                setupVisibilityComponents()
+                if (makeTransactionViewModel.validateTextField(amount)) {
+                    textInputLayoutQunatitySend.error = null
+                    if (makeTransactionViewModel.validateTextField(concept)) {
+                        textInputConceptSend.error = null
+                        sendDataToMakeTransaction(amount, concept)
+                    } else {
+                        textInputConceptSend.error = getString(R.string.error_empty)
+                    }
+                } else {
+                    textInputLayoutQunatitySend.error = getString(R.string.error_empty)
+                }
             }
+
+            setupVisibilityComponents()
         }
     }
 
@@ -100,7 +107,6 @@ class MakeTransactionFragment : Fragment() {
             setOnBackPressedEnable(true)
         }
     }
-
 
     private fun sendDataToMakeTransaction(amount: String, concept: String) {
         makeTransactionViewModel.makeTransactionBody(
