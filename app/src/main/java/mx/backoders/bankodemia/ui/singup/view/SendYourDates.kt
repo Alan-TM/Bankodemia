b4cea82cb22d655f50fc563dc87bccd2538acd29
Alan Torres <alantorresm86@gmail.com>
2022-04-05 10:56:45 -0600

    before merge to alexis branch

--- app/src/main/java/mx/backoders/bankodemia/ui/singup/view/SendYourDates.kt
+++ app/src/main/java/mx/backoders/bankodemia/ui/singup/view/SendYourDates.kt
@@ -23,6 +23,5 @@ class SendYourDates : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        signUpViewModel.justForTest()
     }
 }
